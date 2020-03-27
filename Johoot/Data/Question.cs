19f61c687dde1b0f42e0349ebcaf9338561af7c6
Joanna Lamch <jlamch@gmail.com>
2020-03-27 21:30:43 +0000

    init

---
+++ Johoot/Data/Question.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace Johoot.Data
+{
+    public class Question
+    {
+        public int TimeLimitSeconds { get; set; }
+        public string Text { get; set; }
+        public int Points { get; set; }
+        public List<Answer> Answers { get; set; }
+        public QuestionOptions Options { get; set; }
+        public string ImageUri { get; set; }
+    }
+
+    public class QuestionOptions
+    {
+        public bool HasCorrectAnswer { get; set; }
+        public bool Open { get; set; }
+    }
+
+    public class Answer 
+    {
+        public string Text { get; set; }
+        public bool? IsCorrect { get; set; }
+    }
+
+    public class Quize
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Question> Questions { get; set; }
+    }
+  
+
+}
+
