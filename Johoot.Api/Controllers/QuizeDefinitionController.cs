01591ea5308a98db90aa071c0abd9efbaab6a398
Joanna Lamch <jlamch@gmail.com>
2020-04-01 19:46:46 +0000

    quize controller and repo interface

---
+++ Johoot.Api/Controllers/QuizeDefinitionController.cs
@@ -0,0 +1,67 @@
+﻿using Johoot.Data;
+using Johoot.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Johoot.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuizeDefinitionController : ControllerBase
+    {
+        private readonly ILogger<QuizeDefinitionController> _logger;
+        private readonly IQuizeRepository _repository;
+
+        public QuizeDefinitionController(
+            ILogger<QuizeDefinitionController> logger,
+            IQuizeRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IList<Quize>>> GetAll()
+        {
+            return Ok(await _repository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Quize>> Get(long id)
+        {
+            return Ok(await _repository.GetQuize(id));
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Quize>> Create(Quize item)
+        {
+            //do some conversion from dto? no we use shared model for now
+
+            var created = await _repository.CreateQuize(item);
+            return CreatedAtAction(
+                nameof(Quize),
+                new { id = created.Id },
+                created);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(long id, Quize item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            var updated = await _repository.UpdateQuize(item);
+            if (updated == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ Johoot.Domain/IQuizeRepository.cs
@@ -0,0 +1,17 @@
+﻿using Johoot.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Johoot.Domain
+{
+    public interface IQuizeRepository
+    {
+        Task<IList<Quize>> GetAll();
+
+        Task<Quize> GetQuize(long id);
+
+        Task<Quize> CreateQuize(Quize item);
+
+        Task<Quize> UpdateQuize(Quize item);
+    }
+}
