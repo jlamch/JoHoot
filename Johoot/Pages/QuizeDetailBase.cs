7bee90a70f12d6465b4b3ad1ba48ce1ddcdb466a
Joanna Lamch <jlamch@gmail.com>
2020-04-11 16:24:29 +0000

    first pages

---
+++ Johoot/Pages/QuizeDetailBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Johoot.Pages
+{
+  public class QuizeDetailBase : ComponentBase
+  {
+    [Parameter]
+    public string QuizeId { get; set; }
+
+  }
+}
---
+++ Johoot/Pages/QuizesOverviewBase.cs
@@ -0,0 +1,19 @@
+﻿using Johoot.Data;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Johoot.Pages
+{
+  public class QuizesOverviewBase : ComponentBase
+  {
+    public IList<Quize> Quizes { get; set; }
+
+    protected override Task OnInitializedAsync()
+    {
+      //initialize 
+      Quizes = new List<Quize>();
+      return base.OnInitializedAsync();
+    }
+  }
+}
